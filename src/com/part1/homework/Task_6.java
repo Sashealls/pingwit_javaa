3d8c86b06ae6aeb11d1afe2c396a195217c7bf37
Sasha Vecherynskaja <sashavecherynskaja@Noutbuk-Sasha.local>
2025-10-22 13:51:33 +0300

    inital commit

---
+++ src/com/part1/homework/Task_6.java
@@ -0,0 +1,13 @@
+package com.part1.homework;
+
+public class Task_6 {
+    public static void main( String[] args ) {
+        System.out.println( 3 * 6);
+
+        // 2 вариант
+
+        int a = 5;
+        int b = 5;
+        System.out.println( a * b );
+    }
+}
---
+++ src/com/part1/homework/Task_7.java
@@ -0,0 +1,7 @@
+package com.part1.homework;
+
+public class Task_7 {
+    public static void main(String[] args) {
+        System.out.println( 5 % 2 );
+    }
+}
