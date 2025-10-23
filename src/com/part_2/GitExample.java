d8a28b2c888fc9f7f7a3fb7748a79636f0308e9a
Sashealls <114533439+Sashealls@users.noreply.github.com>
2025-10-23 22:24:14 +0300

    Update GitExample.java

--- src/com/part_2/GitExample.java
+++ src/com/part_2/GitExample.java
@@ -2,6 +2,6 @@ package com.part_2;
 
 public class GitExample {
     public static void main(String[] args) {
-        System.out.println( "hello from git example");
+        System.out.println( "hello from git example comment");
     }
 }
