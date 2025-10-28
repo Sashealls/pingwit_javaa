8c96b7d562ed96f1e891a81317a455641c2f56a3
Alexander Fomin <rezviybelorus@gmail.com>
2025-10-28 07:50:34 +0100

    [Check] Homework 1

--- src/com/part1/homework/Task_5.java
+++ src/com/part1/homework/Task_5.java
@@ -4,6 +4,7 @@ public class Task_5 {
     static void main(String[] args) {
         System.out.println(5 + 3);
 
+        // вариант 2 более предпочтителен, так как использует переменные для хранения значений
         // 2 вариант
         int a = 5;
         int b = 5;
--- src/com/part1/homework/Task_6.java
+++ src/com/part1/homework/Task_6.java
@@ -4,8 +4,9 @@ public class Task_6 {
     static void main(String[] args) {
         System.out.println(3 * 6);
 
+        // вариант 2 более предпочтителен, так как использует переменные для хранения значений
         // 2 вариант
-
+// лишняя строка, давай удалим
         int a = 5;
         int b = 5;
         System.out.println(a * b);
